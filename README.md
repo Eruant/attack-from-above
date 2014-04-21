12cd4773f57e4e0821f8dc6255fe164338709b13
Matt Gale <matt@littleball.co.uk>
2014-04-21 21:22:48 +0100

    removing src files

--- README.md
+++
@@ -1,8 +0,0 @@
-# Attack from above
-
-A work in progress scrolling shooter
-
-## Todo
-
-- add music
-- add more enemies
--- src/js/game.js
+++
@@ -1,5 +0,0 @@
-var Phaser = require('phaser');
-
-var game = new Phaser.Game(320, 480, Phaser.AUTO, 'content', null);
-
-module.exports = game;
