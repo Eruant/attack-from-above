cb648bf13e55b816fac9c84c1bbd32875a237b1a
Matt Gale <matt@littleball.co.uk>
2014-04-21 17:59:47 +0100

    Added sounds

--- js/bundle.js
+++ js/bundle.js
@@ -8,7 +8,6 @@
 
 /*globals require*/
 
-
 var game = require('./game'),
     boot = require('./scenes/boot.js'),
     preloader = require('./scenes/preloader'),
@@ -83,7 +82,7 @@ module.exports = {
     this.bullets = game.add.group();
     this.bullets.enableBody = true;
     this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    this.bullets.createMultiple(20, 'game_sprites', 1);
+    this.bullets.createMultiple(20, 'bullets', 1);
     this.bullets.setAll('checkWorldBounds', true);
     this.bullets.setAll('outOfBoundsKill', true);
 
@@ -102,7 +101,7 @@ module.exports = {
 
     this.score = 0;
     this.style = {
-      font: '30px Arial',
+      font: '30px "VT323"',
       fill: '#fff',
       align: 'center'
     };
@@ -114,15 +113,16 @@ module.exports = {
     this.infoLabel = game.add.text(game.world.centerX, game.world.centerY - 15, "Wave: 1", this.style);
     this.infoLabel.anchor.set(0.5, 0);
     game.time.events.add(1000, this.removeInfoLabel, this);
+
+    this.sfx = game.add.audio('sfx');
+    this.sfx.addMarker('shoot', 0, 0.3);
+    this.sfx.addMarker('enemyExplode', 0.3, 0.4);
+    this.sfx.addMarker('playerExplode', 0.8, 1.3);
   },
 
   update: function () {
     if (this.player.inWorld === false) {
       this.restartGame();
-    }
-
-    if (this.player.body.position.x < 0 || this.player.body.position.x > 290) {
-      this.player.body.velocity.x = -(this.player.body.velocity.x * 0.5);
     }
 
     game.physics.arcade.overlap(this.player, this.enemys, this.restartGame, null, this);
@@ -142,6 +142,16 @@ module.exports = {
       this.waveComplete();
     }
 
+    if (this.player.body.position.x < 0 || this.player.body.position.x > 290) {
+      this.player.body.velocity.x = -(this.player.body.velocity.x * 0.5);
+
+      if (this.player.body.position.x < 0) {
+        this.player.body.position.x = 0;
+      } else if (this.player.body.position.x > 290) {
+        this.player.body.position.x = 290;
+      }
+    }
+
   },
 
   distroyEnemy: function (bullet, enemy) {
@@ -149,19 +159,21 @@ module.exports = {
     enemy.damage(20);
     if (!enemy.alive) {
       this.score += 1;
+      this.sfx.play('enemyExplode');
     }
   },
 
   shoot: function () {
     var x, y, bullet;
 
-    x = this.player.body.position.x;
+    x = this.player.body.position.x + 13;
     y = this.player.body.position.y;
 
     bullet = this.bullets.getFirstDead();
     if (bullet) {
       bullet.reset(x, y);
       bullet.body.velocity.y = -200;
+      this.sfx.play('shoot');
     }
   },
 
@@ -192,14 +204,14 @@ module.exports = {
   waveComplete: function () {
     this.wave.complete = true;
     game.time.events.remove(this.enemyTimer);
-    game.time.events.add(Phaser.Timer.SECOND * 4, this.startNewWave, this);
+    game.time.events.add(Phaser.Timer.SECOND * 6, this.startNewWave, this);
   },
 
   startNewWave: function () {
     this.wave = new Wave(this.currentWave); // send in previous wave
 
     this.currentWave = this.wave.currentWave;
-    this.infoLabel.setText("Wave " + this.currentWave);
+    this.infoLabel.setText("Wave " + this.currentWave.toString());
     this.infoLabel.alpha = 1;
     game.time.events.add(1000, this.removeInfoLabel, this);
 
@@ -216,6 +228,7 @@ module.exports = {
   },
 
   restartGame: function () {
+    this.sfx.play('playerExplode');
 
     var previousHighscore = localStorage.getItem("highscore");
     if (!previousHighscore || previousHighscore < this.score) {
@@ -244,7 +257,7 @@ module.exports = {
       highscore = localStorage.getItem("highscore"),
       lastscore = localStorage.getItem("lastscore"),
       style = {
-        font: '30px Arial',
+        font: '30px "VT323"',
         fill: '#fff',
         align: 'center'
       };
@@ -304,7 +317,12 @@ module.exports = {
 
     this.game.load.image('menu_background', 'assets/menu_background.png');
     this.game.load.spritesheet('game_sprites', 'assets/game_sprites.png', 32, 32);
+    this.game.load.spritesheet('bullets', 'assets/bullets.png', 4, 4);
 
+    this.game.load.audio('sfx', [
+      'assets/fx/sounds.mp3',
+      'assets/fx/sounds.wav'
+    ]);
   },
 
   create: function () {
